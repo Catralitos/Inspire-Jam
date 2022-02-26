28265d8d00adb7af87fc12a592c06962c9c4aa2e
Carlos Marques <catralitos11@gmail.com>
2022-02-26 12:35:40 +0000

    Comecei a trabalhar no segundo inimigo

---
+++ Assets/Scripts/Combat/Units/FishEnemy.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace Combat.Units
+{
+    public class FishEnemy : Enemy
+    {
+        public override UnitMoves.Move ChooseMove(Player playerUnit, UnitMoves.Move lastMove,
+            UnitMoves.Move lastPlayerMove)
+        {
+
+            bool needsHeal = 1.0f * currentHp / maxHp <= healthPercentageToHeal;
+            float x = Random.Range(0.0f, 1.0f);
+
+            if (currentSpeed <= speed || currentSpeed <= playerUnit.currentSpeed)
+            {
+                return x > 0.5 ? UnitMoves.Move.SpeedUp : UnitMoves.Move.SpeedDown;
+            }
+
+            if (!needsHeal && (lastPlayerMove == UnitMoves.Move.Attack || lastPlayerMove == UnitMoves.Move.PhysicalFish ||
+                               lastPlayerMove == UnitMoves.Move.PhysicalVegetables))
+            {
+                return UnitMoves.Move.Counter;
+            }
+
+            if (lastPlayerMove == UnitMoves.Move.DefendMeat || lastPlayerMove == UnitMoves.Move.AbsorbMeat)
+            {
+                return UnitMoves.Move.Attack;
+            }
+            
+            if (needsHeal)
+            {
+                return x > 0.85f ? UnitMoves.Move.Vampire : UnitMoves.Move.Heal;
+            } 
+
+            return UnitMoves.Move.PhysicalFish;
+        }
+    }
+}
