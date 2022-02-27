f1276d7d5a35ef22346559a78462beb8e2f1c545
Carlos Marques <catralitos11@gmail.com>
2022-02-27 21:27:32 +0000

    Creditos feitos

---
+++ Assets/Scripts/CreditsManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using Audio;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditsManager : MonoBehaviour
+{
+    public Button backButton, exitButton;
+    
+    private GameManager _gameManager;
+    private AudioManager _audioManager;
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _audioManager = GetComponent<AudioManager>();
+        _gameManager = GameManager.Instance;
+        backButton.onClick.AddListener(LoadTitle);
+        exitButton.onClick.AddListener(ExitGame);
+    }
+
+    private void LoadTitle()
+    {
+        _audioManager.Play("ButtonClick");
+        _gameManager.LoadNextScene("MainMenu");
+    }
+
+    private void ExitGame()
+    {
+        _audioManager.Play("ButtonClick");
+        Application.Quit();
+    }
+   
+}
